c68ddd9b97267f1d21449e6ecb9ab790f5a342fd
VXette <v.xette@gmail.com>
2016-11-22 18:47:08 +1000

    Simple Pong Game - Single Player

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,80 +10,80 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+	
+	var ball = SKSpriteNode()
+	var enemy = SKSpriteNode()
+	var main = SKSpriteNode()
+	
+	var mainScore = SKLabelNode()
+	var enemyScore = SKLabelNode()
+	
+	var score = [0, 0]
+	
+	let impulse = 15
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+	override func didMove(to view: SKView) {
+		ball = self.childNode(withName: "ball") as! SKSpriteNode
+		enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+		main = self.childNode(withName: "main") as! SKSpriteNode
+		
+		mainScore = self.childNode(withName: "mainScore_label") as! SKLabelNode
+		enemyScore = self.childNode(withName: "enemyScore_label") as! SKLabelNode
+
+		ball.physicsBody?.applyImpulse(CGVector(dx: impulse, dy: impulse))
+
+		let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+		border.friction = 0
+		border.restitution = 1
+
+		self.physicsBody = border
+	}
+	
+	func startGame() {
+		score = [0, 0]
+		mainScore.text = String(score[0])
+		enemyScore.text = String(score[1])
+	}
+	
+	func addScore(playerWhoWon :SKSpriteNode) {
+		ball.position = CGPoint(x: 0, y: 0)
+		ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+		
+		if playerWhoWon == main {
+			score[0] += 1
+			mainScore.text = String(score[0])
+			ball.physicsBody?.applyImpulse(CGVector(dx: impulse, dy: impulse))
+		} else if playerWhoWon == enemy {
+			score[1] += 1
+			enemyScore.text = String(score[1])
+			ball.physicsBody?.applyImpulse(CGVector(dx: -1 * impulse, dy: -1 * impulse))
+		}
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for touch in touches {
+			let location = touch.location(in: self)
+			
+			main.run(SKAction.moveTo(x: location.x, duration: 0.4))
+		}
+	}
+
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for touch in touches {
+			let location = touch.location(in: self)
+			
+			main.run(SKAction.moveTo(x: location.x, duration: 0.4))
+		}
+	}
+	
+	override func update(_ currentTime: TimeInterval) {
+		enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.8))
+		
+		if ball.position.y <= main.position.y - 70 {
+			addScore(playerWhoWon: enemy)
+		} else if ball.position.y >= enemy.position.y + 70 {
+			addScore(playerWhoWon: main)
+		}
+	}
+	
 }
